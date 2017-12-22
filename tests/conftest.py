6e874ed2c37a29fee6338909c96b226660cd6fa6
Greg Sebastian <g.s.seabass@gmail.com>
2017-12-21 20:38:18 -0800

    Properly parametrized the create dependencies so that all subsequent tests are dependent on all create tests passing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,29 +1,11 @@
 import os
 import pytest
+
 from geomark import config
+from . import data
 
 
-_geomark_ids = ['gm-abcdefghijklmnopqrstuvwxyz0000bc', 'gm-abcdefghijklmnopqrstuv0bcislands']
-
-# The expected geometries for each test file of the matching geom_type
-_geo_features = {
-    'point': {'type': 'Feature', 'crs': {'type': 'name', 'properties': {'name': 'urn:ogc:def:crs:EPSG::4326'}}, 'geometry': {'type': 'Point', 'coordinates': [-123.37870723365188, 49.96295634587292]}, 'properties': {'geometryType': 'Point', 'numPolygons': 1, 'numParts': 1, 'minX': -123.37870723365188, 'minY': 49.96295634587292, 'maxX': -123.37870723365188, 'maxY': 49.96295634587292, 'centroidX': -123.37870723365188, 'centroidY': 49.96295634587292, 'numVertices': 1, 'length': 0, 'area': 0, 'isValid': True, 'isSimple': True, 'isRobust': True, 'minimumClearance': None}},
-    'linestring': {'type': 'Feature', 'crs': {'type': 'name', 'properties': {'name': 'urn:ogc:def:crs:EPSG::4326'}}, 'geometry': {'type': 'LineString', 'coordinates': [[-123.27582683416017, 50.03973275790413], [-123.53226005267769, 49.79711929326573]]}, 'properties': {'geometryType': 'LineString', 'numPolygons': 1, 'numParts': 1, 'minX': -123.53226005267769, 'minY': 49.79711929326573, 'maxX': -123.27582683416017, 'maxY': 50.03973275790413, 'centroidX': -123.40404344341893, 'centroidY': 49.918426025584935, 'numVertices': 2, 'length': 32669.339029919942, 'area': 0, 'isValid': True, 'isSimple': True, 'isRobust': True, 'minimumClearance': 32669.34}},
-    'polygon': {'type': 'Feature', 'crs': {'type': 'name', 'properties': {'name': 'urn:ogc:def:crs:EPSG::4326'}}, 'geometry': {'type': 'Polygon', 'coordinates': [[[-123.47698103651241, 50.048945926824196], [-123.59828776745799, 50.030519590901186], [-123.59828777134982, 49.910748381756406], [-123.42937966256794, 49.74951791779005], [-123.1284161225308, 49.92149708232013], [-123.47698103651241, 50.048945926824196]]]}, 'properties': {'geometryType': 'Polygon', 'numPolygons': 1, 'numParts': 1, 'minX': -123.59828777134982, 'minY': 49.74951791779005, 'maxX': -123.1284161225308, 'maxY': 50.048945926824196, 'centroidX': -123.40793678392268, 'centroidY': 49.914070995317516, 'numVertices': 6, 'length': 101540.46853039399, 'area': 621899992, 'isValid': True, 'isSimple': True, 'isRobust': True, 'minimumClearance': 8928.062}}
-}
-
-# The shapes resulting from each {shape_type}.{format} should have the same shapes stored in different formats
-_geo_files = [
-    {'format': 'kml', 'file': 'point.kml', 'expected_geom': _geo_features['point']},
-    {'format': 'kml', 'file': 'line.kml', 'expected_geom': _geo_features['linestring']},
-    {'format': 'kml', 'file': 'polygon.kml', 'expected_geom': _geo_features['polygon']},
-    {'format': 'geojson', 'file': 'point.geojson', 'expected_geom': _geo_features['point']},
-    {'format': 'geojson', 'file': 'line.geojson', 'expected_geom': _geo_features['linestring']},
-    {'format': 'geojson', 'file': 'polygon.geojson', 'expected_geom': _geo_features['polygon']}
-]
-
-
-@pytest.fixture(scope='module', params=_geomark_ids)
+@pytest.fixture(scope='module', params=data.geomark_ids)
 def geomark_id(request):
     gm_id = request.param
     yield gm_id
@@ -31,10 +13,10 @@ def geomark_id(request):
 
 @pytest.fixture(scope='module')
 def geomark_ids(request):
-    yield _geomark_ids
+    yield data.geomark_ids
 
 
-@pytest.fixture(scope='module', params=_geomark_ids)
+@pytest.fixture(scope='module', params=data.geomark_ids)
 def geomark_url(request):
     gm_url = config.GEOMARK_ID_BASE_URL.format(
         protocol='http',
@@ -43,7 +25,7 @@ def geomark_url(request):
     yield gm_url
 
 
-@pytest.fixture(scope='module', params=_geomark_ids)
+@pytest.fixture(scope='module', params=data.geomark_ids)
 def geomark_https_url(request):
     gm_url = config.GEOMARK_ID_BASE_URL.format(
         protocol='https',
@@ -52,20 +34,8 @@ def geomark_https_url(request):
     yield gm_url
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope='module', params=data.geo_files)
 def geo_file(request):
-    filename = request.module.__file__
-    test_dir, _ = os.path.splitext(filename)
-    with open(os.path.join(test_dir, _geo_files[0]['file']), 'r') as f:
-        yield {
-            'format': _geo_files[0]['format'],
-            'data': f.read(),
-            'expected_geom': _geo_files[0]['expected_geom']
-        }
-
-
-@pytest.fixture(scope='module', params=_geo_files[1:])
-def geo_files(request):
     filename = request.module.__file__
     test_dir, _ = os.path.splitext(filename)
     with open(os.path.join(test_dir, request.param['file']), 'r') as f:
--- tests/test_geomark.py
+++ tests/test_geomark.py
@@ -1,7 +1,9 @@
 import pytest
 import json
+
 from geomark.geomark import Geomark
 from geomark.config import LOGGER as logger
+from . import data as _data
 
 
 def strip_variable_properties(data, method='feature'):
@@ -14,7 +16,6 @@ def strip_variable_properties(data, method='feature'):
         return data
 
 
-@pytest.mark.dependency()
 def test_create(geo_file):
     # Perhaps the use of the Geomark.feature() method makes this sort of double as a test_feature test...
     expected = geo_file['expected_geom']  # variable properties have already been removed.
@@ -25,53 +26,43 @@ def test_create(geo_file):
     assert expected == geojson
 
 
-@pytest.mark.dependency(depends=["test_create"])
-def test_create_other_formats(geo_files):
-    expected = geo_files['expected_geom']  # variable properties have already been removed.
-
-    gm = Geomark.create(format=geo_files['format'], body=geo_files['data'])
-    geojson = strip_variable_properties(json.loads(gm.feature('geojson')))
-
-    assert expected == geojson
-
-
-@pytest.mark.dependency(depends=["test_create"])
+@pytest.mark.dependency(depends=_data.depends_create)
 def test_bbox(geomark_id):
     gm = Geomark(geomarkId=geomark_id)
     assert gm.boundingBox() is not None
 
 
-@pytest.mark.dependency(depends=["test_create"])
+@pytest.mark.dependency(depends=_data.depends_create)
 def test_feature(geomark_id):
     gm = Geomark(geomarkId=geomark_id)
     assert gm.feature()
 
 
-@pytest.mark.dependency(depends=["test_create"])
+@pytest.mark.dependency(depends=_data.depends_create)
 def test_info(geomark_id):
     gm = Geomark(geomarkId=geomark_id)
     assert gm.info()
 
 
-@pytest.mark.dependency(depends=["test_create"])
+@pytest.mark.dependency(depends=_data.depends_create)
 def test_parts(geomark_id):
     gm = Geomark(geomarkId=geomark_id)
     assert gm.boundingBox()
 
 
-@pytest.mark.dependency(depends=["test_create"])
+@pytest.mark.dependency(depends=_data.depends_create)
 def test_point(geomark_id):
     gm = Geomark(geomarkId=geomark_id)
     assert gm.point()
 
 
-@pytest.mark.dependency(depends=["test_create"])
+@pytest.mark.dependency(depends=_data.depends_create)
 def test_copy(geomark_id):
     gm = Geomark(geomarkId=geomark_id)
     assert gm.copy()
 
 
-@pytest.mark.dependency(depends=["test_create"])
+@pytest.mark.dependency(depends=_data.depends_create)
 def test_copy_multiple(geomark_ids):
     gm1 = Geomark(geomarkId=geomark_ids[0])
     assert gm1.copy(geomarkUrl=[geomark_ids[0], geomark_ids[1]], allowOverlap=True, bufferMetres=0.1)
